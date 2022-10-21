2eb4c5e4026b787a585b16e7e8743e6a63e71cf0
Arun93516 <arunkumar93516@gmail.com>
2022-10-21 12:58:25 +0530

    Added: Here we can add the Employee is present parttime working also In EmployeeWage

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -10,19 +10,29 @@ public class EmployeeWageComputationProblem {
 		Random random = new Random();
 		int PerhourWage = 20;
 		int FullDay = 8;
+		int Parttime = 4;
+
 		int Salary;
-		
-		int randomNumber = random.nextInt(2);
+
+		int randomNumber = random.nextInt(3);
 		switch (randomNumber) {
-		case 1:
-			System.out.println("Employee is present Full Day  ");
-			Salary = PerhourWage * FullDay; 
-			System.out.println("Salary for Full day is :"+Salary);
-			break;
 		case 0:
 			System.out.println("Employee is Absent Today ");
 			Salary = PerhourWage * 0;
-			System.out.println("Salary for Absent  day is :"+Salary);
+			System.out.println("Salary for Absent  day is :" + Salary);
+		case 1:
+
+			System.out.println("Employee is present Full Day  ");
+			Salary = PerhourWage * FullDay;
+			System.out.println("Salary for Full day is :" + Salary);
+			break;
+		case 2:
+
+			System.out.println("Employee is Present Parttime   ");
+			Salary = PerhourWage * Parttime;
+			System.out.println("Salary for Partime day is :" + Salary);
+			break;
+
 		}
 	}
 }
